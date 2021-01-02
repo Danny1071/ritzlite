a02bdce928124306d908be3fb3140f2712e206a1
Danny <Dannyblackbourn10@gmail.com>
2021-01-02 18:05:31 +0000

    Hide special loot in cox

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/coxspecialloothider/CoxSpecialLootHiderConfig.java
@@ -0,0 +1,12 @@
+package net.runelite.client.plugins.coxspecialloothider;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+
+/**
+ * @author Danny
+ */
+@ConfigGroup("example")
+public interface CoxSpecialLootHiderConfig extends Config {
+    //The only setting needed is the plugin being turned on
+}
